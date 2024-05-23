5d0d990247be0f989a0ec8e79a0fb179ae0c39b9
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-05-23 16:00:14 +0200

    feat: Dockerize apps

--- angular/rent-app/webpack.config.js
+++ angular/rent-app/webpack.config.js
@@ -10,7 +10,32 @@ module.exports = {
       exposes: {
         "./Component": "./src/app/app.component.ts",
       },
-      shared: ["@angular/core", "@angular/common", "@angular/router"],
+      shared: [
+        {
+          "@angular/core": {
+            eager: true,
+            singleton: true,
+            strictVersion: true,
+            requiredVersion: "^17.3.0",
+          },
+        },
+        {
+          "@angular/common": {
+            eager: true,
+            singleton: true,
+            strictVersion: true,
+            requiredVersion: "^17.3.0",
+          },
+        },
+        {
+          "@angular/router": {
+            eager: true,
+            singleton: true,
+            strictVersion: true,
+            requiredVersion: "^17.3.0",
+          },
+        },
+      ],
     }),
   ],
 };
--- host-app/webpack.config.js
+++ host-app/webpack.config.js
@@ -13,7 +13,6 @@ module.exports = {
       remotes: {
         rentApp: 'rentApp@http://localhost:3001/remoteEntry.js',
         saleApp: 'saleApp@http://localhost:3002/remoteEntry.js',
-        sharedCss: 'sharedCss@http://localhost:3003/remoteEntry.js',
       },
     }),
   ],
