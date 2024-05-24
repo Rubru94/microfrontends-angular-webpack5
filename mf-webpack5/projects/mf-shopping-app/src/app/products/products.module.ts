37247f514bcee3d633fb31109460e44ba1e89308
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-05-24 14:55:51 +0200

    fix: Order project directories

--- mf-webpack5/projects/mf-shopping-app/src/app/products/products.module.ts
+++ mf-webpack5/projects/mf-shopping-app/src/app/products/products.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ProductCardComponent } from './../product-card/product-card.component';
+import { ProductCardComponent } from '../product-card/product-card.component';
 import { ProductsComponent } from './products.component';
 
 import { RouterModule } from '@angular/router';
